39fa07ed6bc6b00add592bc80397e3cc4f6774d5
jh102 <jhonles@gmail.com>
2020-09-07 10:59:25 -0400

    Added the makeCacheMatrix function and description

---
+++ cachematrix.R
@@ -0,0 +1,28 @@
+# This function creates a special "matrix" object that can cache its inverse using function solve()
+# "<<-"  is used to assign a value to an object of a different environment from the current one
+# k is a free variable
+# The solve() function return the inverse of the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+    Ma_solve <<- NULL
+    set <- function(k){
+        x <<- k
+        Ma_solve <<- NULL
+        
+    }
+    
+    get <- function() X
+    Ma_setsolve <- function(solve) Ma_solve <<- solve
+    Ma_getsolve <- function() Ma_solve
+    
+    list(set = set, get = get,
+         Ma_setsolve = Ma_setsolve,
+         Ma_getsolve = Ma_getsolve)
+    
+    
+}
+
+
+
+# This function calculates the inverse of the special "Matrix" returned by makeCacheMatrix()
+
